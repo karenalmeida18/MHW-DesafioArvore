c164349b5440e74f973cad9f28f2b0166ff36da8
Karen <karenkaka2010@hotmail.com>
2020-09-03 17:20:30 -0300

    feat front: adiciona navbar

--- Frontend/src/routes.js
+++ Frontend/src/routes.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-import Home from './pages/home'
+import Main from './pages/main'
 
 export default function Routes() {
     return (
         <BrowserRouter>
             <Switch>
-                <Route path="/" exact component={Home}/>
+                <Route path="/" exact component={Main}/>
             </Switch>
         </BrowserRouter>
 
