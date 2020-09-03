b273c00c41a4f34cb5cd9ca7028d4b2058cd87be
Karen <karenkaka2010@hotmail.com>
2020-09-02 23:42:36 -0300

    feat front: adiciona routes

---
+++ Frontend/src/routes.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+export default function Routes() {
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route />
+            </Switch>
+        </BrowserRouter>
+
+    );
+}
