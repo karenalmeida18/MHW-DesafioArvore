146988f10143669df4a4689b2b4617a01304e6ae
dayana <dayanabritodetoledo@gmail.com>
2020-09-06 00:36:13 -0300

    feat back: exclusão de comentario

--- Backend/src/database/index.js
+++ Backend/src/database/index.js
@@ -4,12 +4,6 @@ const dbConfig = require('../config/database');
 const connection = new Sequelize(dbConfig);
 const Recompense = require('../models/Recompense');
 
-//const bodyParser = require('body-parser');
-
-//app.use(bodyParser.urlencoded({ extended: false }));
-//app.use(bodyParser.json());
 Recompense.init(connection);
-
-//Recompense.create({description: 'Tomar sorvete depois do jantar'});
 
 module.exports = connection;
